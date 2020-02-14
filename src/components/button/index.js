8d87ceeee11d68734a8a9fcd6d15c67092ecd7c3
AustinLynes <austinlynes94@gmail.com>
2020-02-14 08:59:47 -0800

    added propertys to button component testing...

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { BUTTON } from './style.js'
-const Button = () => {
-    return <BUTTON />
+const Button = (props) => {
+    const { name, onClickCallback, ...rest } = props
+    
+    return <BUTTON name={name} value={name} onClick={onClickCallback} />
 }
 
 export default Button
