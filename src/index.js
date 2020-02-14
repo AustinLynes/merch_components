c4c08a740e04aeb1f6e17654eeb2e6da642d07fd
AustinLynes <austinlynes94@gmail.com>
2020-02-13 19:23:41 -0800

    super hotfix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Item from "./components/item/";
 import Container from './components/container'
-import Carosel from './components/navigation'
-import Navigation from './components/carosel'
+import Carosel from './components/carosel'
+import Navigation from './components/navigation'
 
 export {
     Item,
