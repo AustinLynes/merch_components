2e16b539b47ccbb8b122f1372fa122f9f06960cd
AustinLynes <austinlynes94@gmail.com>
2020-02-15 10:19:43 -0800

    added basic header gonna add styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,7 @@
 // Search Bar
 // Cart Button
 import React from 'react'
-import { HEADER } from './style'
+import { HEADER, SEARCH_BAR, QUERY } from './style'
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -12,14 +12,13 @@ const Header = (props) => {
 
     const { title, ...rest } = props;
 
-
     return (
         <HEADER>
             <h2>{title}</h2>
-            <div>
-                <input />
-                <FontAwesomeIcon icon={faSearch} />
-            </div>
+            <SEARCH_BAR>
+                <QUERY />
+                <SEARCH_BUTTON icon={faSearch} />
+            </SEARCH_BAR>
         </HEADER>
     )
 }
--- src/components/header/style.js
+++ src/components/header/style.js
@@ -1,6 +1,21 @@
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const HEADER = styled.div`
     width:100%;
     padding:1rem;
+    background:#333;
 `
+export const SEARCH_BAR = styled.div`
+    padding:5px;
+    border:2px solid black;
+    border-radius:10px;
+`
+const QUERY = styled.input`
+    border:none;
+    font-size:1.4rem;
+`
+ export const SEARCH_BUTTON = styled(FontAwesomeIcon)`
+    color: grey;
+    font-size:1.4rem;
+ `
