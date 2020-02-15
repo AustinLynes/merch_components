c4ce352622d1caef9c4d74f30b4ed3e9caebdd0c
AustinLynes <austinlynes94@gmail.com>
2020-02-15 10:33:36 -0800

    adding styles

--- src/components/header/style.js
+++ src/components/header/style.js
@@ -2,33 +2,42 @@ import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const HEADER = styled.div`
+    box-sizing:border-box;
     display:flex;
     flex-flow:row wrap;
+    justify-content:space-between;
     width:100%;
     padding:1rem;
     background:#333;
 `
 export const TITLE = styled.h2`
-    width:40%;
+    width:20%;
     color:white;
     margin:0;
 `
 export const SEARCH_BAR = styled.div`
     background:white;
+    display:flex;
+    flex-flow:row wrap;
+    align-items:center;
+    width:20%;
     padding:5px;
     border:1px solid grey;
     border-radius:10px;
 `
 export const QUERY = styled.input`
+    width:90%;
     border:none;
     font-size:1.4rem;
 `
 export const SEARCH_BUTTON = styled(FontAwesomeIcon)`
     color: grey;
+    width:5%;
+    padding:10px;
     font-size:1.4rem;
  `
 
  export const CART_BUTTON = styled(FontAwesomeIcon)`
     color:dodgerblue;
-    font-size:3rem;
+    font-size:5rem;
  `
