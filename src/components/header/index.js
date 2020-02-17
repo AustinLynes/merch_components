5060d0a43f97c4a77d4c389261a1d7c91f009f7e
AustinLynes <austinlynes94@gmail.com>
2020-02-17 14:46:47 -0800

    added dynamic request from team

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -9,14 +9,14 @@ const Header = (props) => {
     const { _header, _logo, _title, _searchBar, _query, _searchIcon, _cartIcon } = style
    
     return (
-        <HEADER style={_header}>
-            <LOGO style={_logo} src={logoSrc} />
-            <TITLE style={_title}>{title}</TITLE>
-            <SEARCH_BAR style={_searchBar}>
-                <QUERY style={_query} />
-                <SEARCH_BUTTON style={_searchIcon} icon={faSearch} />
+        <HEADER style={style && _header}>
+            <LOGO style={style && _logo} src={logoSrc} />
+            <TITLE style={style &&_title}>{title}</TITLE>
+            <SEARCH_BAR style={style &&_searchBar}>
+                <QUERY style={style &&_query} />
+                <SEARCH_BUTTON style={style &&_searchIcon} icon={faSearch} />
             </SEARCH_BAR>
-            <CART_BUTTON style={_cartIcon} icon={faShoppingCart} />
+            <CART_BUTTON style={style &&_cartIcon} icon={faShoppingCart} />
         </HEADER>
     )
 }
