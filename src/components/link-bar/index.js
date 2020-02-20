4b4863f00037532662ab12670a1051241fe8882f
AustinLynes <austinlynes94@gmail.com>
2020-02-19 19:38:24 -0800

    adding changes...

--- src/components/link-bar/index.js
+++ src/components/link-bar/index.js
@@ -7,26 +7,21 @@ import { ICON } from '../global/style.js'
 
 const LinkBar = (props) => {
     const { links, style, ...rest } = props;
-    const checkName = (n) => {
-        var i = null;
-        switch (n) {
-            case "facebook": return i = faFacebook;
-            case "github": return i = faGit;
-            case "twitter": return i = faTwitter;
-            case 'twitch': return i = faTwitch;
-        }
-        return i;
-    }
-
+    
     return (
         <LINKBAR>
             {
                 links && links.map((link, i) => (
                     <LINKCARD key={i}>
                         <ICON style={style && style.iconStyle} icon={() => {
-                            console.log(link.name);
-                            return checkName(link.name);
-                        }} />
+                            switch (link.name) {
+                                case "facebook": return faFacebook;
+                                case "github": return faGit;
+                                case "twitter": return faTwitter;
+                                case 'twitch': return faTwitch;
+                            }
+                        }
+                        } />
                         <LINK>{link.name}</LINK>
                     </LINKCARD>
                 ))
