6a4bfc4837dc0f029f40330b5b9bcd3d7e9b6def
AustinLynes <austinlynes94@gmail.com>
2020-02-19 19:44:28 -0800

    still updating link bar

--- src/components/link-bar/index.js
+++ src/components/link-bar/index.js
@@ -7,20 +7,17 @@ import { ICON } from '../global/style.js'
 
 const LinkBar = (props) => {
     const { links, style, ...rest } = props;
-    
+
     return (
         <LINKBAR>
             {
                 links && links.map((link, i) => (
                     <LINKCARD key={i}>
-                        <ICON style={style && style.iconStyle} icon={() => {
-                            switch (link.name) {
-                                case "facebook": return faFacebook;
-                                case "github": return faGit;
-                                case "twitter": return faTwitter;
-                                case 'twitch': return faTwitch;
-                            }
-                        }
+                        <ICON style={style && style.iconStyle} icon={
+                            link.name === 'facebook' ? faFacebook
+                                : link.name === 'github' ? faGit
+                                    : link.name === 'twitter' ? faTwitter
+                                        : null
                         } />
                         <LINK>{link.name}</LINK>
                     </LINKCARD>
