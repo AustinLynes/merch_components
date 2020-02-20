e7c9014ef63b95a52c053a494bb782e005b52da7
AustinLynes <austinlynes94@gmail.com>
2020-02-19 19:16:14 -0800

    changes to the linkbar... thinking this will work better... added enum check to see what name is.. if i cant find it we cant use it.. sorry

--- src/components/link-bar/index.js
+++ src/components/link-bar/index.js
@@ -2,15 +2,26 @@ import React from 'react'
 
 import { LINKBAR, LINK, LINKCARD, PREVIEW } from './style.js'
 
-const LinkBar = (props) => {
+import { faFacebook, faGit, faTwitter, faTwitch } from '@fortawesome/free-brands-svg-icons'
+import { ICON } from '../global/style.js'
 
+const LinkBar = (props) => {
     const { links, ...rest } = props;
+    const checkName = (__name) => {
+        switch (__name) {
+            case "facebook": return faFacebook;
+            case "github": return faGit;
+            case "twitter":return faTwitter;
+            case 'twitch':return faTwitch; 
+        }
+        return alert('name could not be found...');
+    }
     return (
         <LINKBAR>
             {
                 links && links.map((link, i) => (
                     <LINKCARD key={i}>
-                        <PREVIEW src={link.src}/>
+                        <ICON style={style && style.iconStyle} icon={ () => checkName(link.name) } />
                         <LINK>{link.name}</LINK>
                     </LINKCARD>
                 ))
