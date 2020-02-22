1a5f04ec7af39ed402f6ba6db5844e187d35882c
AustinLynes <austinlynes94@gmail.com>
2020-02-22 11:17:25 -0800

    weekeend updates -- adding default style a item --

--- src/components/item/style.js
+++ src/components/item/style.js
@@ -126,7 +126,7 @@ ${props => {
 // plus icon needs to be turned into its own card.. 
 export const PLACEHOLDER = styled.div`
     display:flex;
-    flex-direction:column;
+    justify-content:center;
     align-items:center;
     border:1px solid lightgrey;
     border-radius:10px;
