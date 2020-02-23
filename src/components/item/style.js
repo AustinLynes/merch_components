eb3408a7b35a92f8e8d1292f8327c94a02c67903
AustinLynes <austinlynes94@gmail.com>
2020-02-23 07:54:11 -0800

    weekeend updates -- adding default style a item --

--- src/components/item/style.js
+++ src/components/item/style.js
@@ -149,8 +149,8 @@ export const PLACEHOLDER = styled.div`
     ${props => {
         if (props.item_is_null) return `
             width:20%;
-            margin:25px;
-            height:642px;
+            margin:20px;
+            height:622px;
         `
         if (props.style) return `
              position:${props.style.position};
