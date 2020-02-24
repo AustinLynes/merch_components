910e1c01b0c6c89cc03e388f41e18a6df482082b
AustinLynes <austinlynes94@gmail.com>
2020-02-24 10:30:29 -0800

    weekeend updates -- adding default style a item --

---
+++ src/components/button/style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const BUTTON = styled.button`
+    /* this is the default styling of the button */
+    padding:1rem;
+    border:none;
+    width:20%;
+    border-radius:10px;
+    margin-top:75px;
+    background:#000;
+    color:white;
+    /* any overriding styles will be displayed here  */
+    ${props => {
+        if (props.style) {
+            return `
+                padding:${props.style.padding};
+                border:${props.style.border};
+                width:${props.style.width};
+                border-radius:${props.style.borderRadius};
+                background-color:${props.style.backgroundColor};
+                color:${props.style.color};
+            `
+        }
+
+    }}
+
+`
