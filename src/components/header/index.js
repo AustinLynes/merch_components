d552ce44e3f93c4a73c82981100c6bbf7b32a86f
AustinLynes <austinlynes94@gmail.com>
2020-02-24 09:53:06 -0800

    weekeend updates -- adding default style a item --

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,12 +5,15 @@ import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Header = (props) => {
 
-    const { style, title, logoSrc, cartCount, ...rest } = props;
+    const { style, title, logoSrc, cartCount, handleChangeCallback, ...rest } = props;
 
+    const HandleChangeCallback = ()=>{
+        return handleChangeCallback && handleChangeCallback();
+    } 
     return (
         <HEADER style={style && style.wrapperStyle}>
             <LOGO style={style && style.logoStyle} src={logoSrc} />
-            <TITLE style={style && style.titleStyle} placeholder={title} />
+            <TITLE style={style && style.titleStyle} placeholder={'Add a Title'} value={title && title} />
             <SEARCH_BAR style={style && style.searchBarStyle}>
                 <QUERY style={style && style.queryStyle} />
                 <SEARCH_BUTTON style={style && style.searchIconStyle} icon={faSearch} />
