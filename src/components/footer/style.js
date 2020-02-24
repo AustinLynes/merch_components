b8b2a1a63982f1ae23f1a36dc79ff4159b29a137
AustinLynes <austinlynes94@gmail.com>
2020-02-24 11:36:43 -0800

    weekeend updates -- adding default style a item --

--- src/components/footer/style.js
+++ src/components/footer/style.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 export const FOOTER = styled.div`
     display:flex;
     justify-content:space-evenly;
-    padding:10px 0;
     margin:0;
     width:100%;
     height:90px;
