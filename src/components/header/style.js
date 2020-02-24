059c939a557925651d7d8f3ea66d8a314b736201
AustinLynes <austinlynes94@gmail.com>
2020-02-24 09:45:41 -0800

    weekeend updates -- adding default style a item --

--- src/components/header/style.js
+++ src/components/header/style.js
@@ -6,7 +6,7 @@ export const HEADER = styled.div`
     display:flex;
     flex-flow:row wrap;
     justify-content:space-between;
-    align-content:center;
+    align-items:center;
     width:100%;
     height:200px;
     max-height:200px;
