668e1771421441eea6bf158742bf1e806d56b94a
AustinLynes <austinlynes94@gmail.com>
2020-02-24 11:11:09 -0800

    weekeend updates -- adding default style a item --

--- src/components/carosel/style.js
+++ src/components/carosel/style.js
@@ -76,24 +76,24 @@ export const ICON = styled(FontAwesomeIcon)`
                         color:white;
 
                 `
-                if (props.style) return `
-                        position:${props.style.position};
-                        bottom:${props.style.bottomPos};
-                        top:${props.style.topPos};
-                        right:${props.style.rightPos};
-                        left:${props.style.leftPos};
-                        padding:${props.style.padding};
-                        width:${props.style.width};
-                        font-size:${props.style.fontSize};
-                        color: black;  // this is black as a fallback... 
-                        -webkit-text-fill-color: ${props.style.textFillColor}; 
-                        -webkit-text-stroke-width: ${props.style.strokeWidth};
-                        -webkit-text-stroke-color: ${props.style.strokeColor};
-                        text-align:${props.style.textAlign};
-                        border:${props.style.border};
-                        background:${props.style.backgroundColor};
-                        margin:${props.style.margin};
-                          `
+                // if (props.style) return `
+                //         position:${props.style.position};
+                //         bottom:${props.style.bottomPos};
+                //         top:${props.style.topPos};
+                //         right:${props.style.rightPos};
+                //         left:${props.style.leftPos};
+                //         padding:${props.style.padding};
+                //         width:${props.style.width};
+                //         font-size:${props.style.fontSize};
+                //         color: black;  // this is black as a fallback... 
+                //         -webkit-text-fill-color: ${props.style.textFillColor}; 
+                //         -webkit-text-stroke-width: ${props.style.strokeWidth};
+                //         -webkit-text-stroke-color: ${props.style.strokeColor};
+                //         text-align:${props.style.textAlign};
+                //         border:${props.style.border};
+                //         background:${props.style.backgroundColor};
+                //         margin:${props.style.margin};
+                //           `
         }}
 `
 export const PLACEHOLDER = styled.div`
