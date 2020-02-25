f39b4e2ca7a2f097bb1dcd0f7ed154df9d902fb6
AustinLynes <austinlynes94@gmail.com>
2020-02-25 07:50:54 -0800

    weekeend updates -- adding default style a item --

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,24 +1,29 @@
 import React, { useState } from 'react'
-import { BUTTON,INPUT } from './style.js'
+import { BUTTON, INPUT, EDIT_ICON, BUTTON_WRAPPER } from './style.js'
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'
 const Button = (props) => {
     const { style, name, onClickCallback, onChangeCallback, ...rest } = props
     const [edit, setEdit] = useState(false)
-    const OnChangeCallback = (e)=>{
+    const OnChangeCallback = (e) => {
         e.preventDefault();
         onChangeCallback && onChangeCallback(e);
     }
-   
+
     const OnClickCallback = (e) => {
         e.preventDefault();
         setEdit(!edit);
         onClickCallback && onClickCallback(e);
     }
 
-    if(!edit){
-        return <BUTTON style={style && style.wrapperStyle} name={name} onClick={OnClickCallback}>{name}</BUTTON>
-    }else{
-        return <INPUT style={style && style.inputStyle} onChange={OnChangeCallback} value={name}/>
-    }
+    return (
+        <BUTTON_WRAPPER>
+            {
+                !edit ?
+                    <BUTTON style={style && style.wrapperStyle} name={name} onClick={OnClickCallback}>{name}</BUTTON>
+                    : <INPUT style={style && style.inputStyle} onChange={OnChangeCallback} value={name} />
+            }
+            <EDIT_ICON icon={faPencilAlt}/>
+        </BUTTON_WRAPPER>
+    )
 }
-
 export default Button
--- src/components/button/style.js
+++ src/components/button/style.js
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export const BUTTON = styled.button`
-    /* this is the default styling of the button */
+// BUTTON_WRAPPER
+
+export const BUTTON_WRAPPER = styled.div`
     border:none;
+    display:flex;
+    flex-flow:row wrap;
+    justify-content:center;
+    align-items:center;
+    align-content:center;
     width:20%;
     border-radius:10px;
     height:90px;
@@ -26,14 +33,42 @@ export const BUTTON = styled.button`
     }}
 
 `
+
+export const BUTTON = styled.button`
+    /* this is the default styling of the button */
+    border:none;
+    flex-grow:1;
+    width:90%;
+    height:100%;
+    border-radius:10px;
+    font-size:1.8rem;
+    background:#000;
+    color:white;
+    /* any overriding styles will be displayed here  */
+    ${props => {
+        if (props.style) {
+            return `
+                padding:${props.style.padding};
+                border:${props.style.border};
+                width:${props.style.width};
+                border-radius:${props.style.borderRadius};
+                background-color:${props.style.backgroundColor};
+                color:${props.style.color};
+            `
+        }
+
+    }}
+
+`
 export const INPUT = styled.input`
-background:black;
-color: white;
-text-align:center;
-width:20%;
-padding:5px;
-border:none;
-font-size:1.8rem;
+ border:none;
+    flex-grow:1;
+    width:90%;
+    height:100%;
+    border-radius:10px;
+    font-size:1.8rem;
+    background:#000;
+    color:white;
 
 ${props => {
         if (props.style) return `
@@ -54,5 +89,30 @@ ${props => {
             background:${props.style.backgroundColor};
             margin:${props.style.margin};
          `
+    }}
+`
+
+export const EDIT_ICON = styled(FontAwesomeIcon)`
+width:5%;
+color:lightgrey;
+${props => {
+        if (props.style) return `
+             position:${props.style.position};
+            bottom:${props.style.bottomPos};
+            top:${props.style.topPos};
+            right:${props.style.rightPos};
+            left:${props.style.leftPos};
+            padding:${props.style.padding};
+            width:${props.style.width};
+            font-size:${props.style.fontSize};
+            color: black;  // this is black as a fallback... 
+            -webkit-text-fill-color: ${props.style.textFillColor}; 
+            -webkit-text-stroke-width: ${props.style.strokeWidth};
+            -webkit-text-stroke-color: ${props.style.strokeColor};
+            text-align:${props.style.textAlign};
+            border:${props.style.border};
+            background:${props.style.backgroundColor};
+            margin:${props.style.margin};
+    `
     }}
 `
