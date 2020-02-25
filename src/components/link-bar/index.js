015dc63a94b978356f3fbef69dc227401e2cd917
AustinLynes <austinlynes94@gmail.com>
2020-02-24 19:56:30 -0800

    weekeend updates -- adding default style a item --

--- src/components/link-bar/index.js
+++ src/components/link-bar/index.js
@@ -1,31 +1,38 @@
 import React from 'react'
 
-import { LINKBAR, LINK, LINKCARD, PREVIEW,ICON } from './style.js'
+import { LINKBAR, LINK, LINKCARD, PLACEHOLDER, PLUS_ICON, ICON } from './style.js'
 
 import { faFacebook, faGithub, faTwitter, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
 const LinkBar = (props) => {
     const { links, style, ...rest } = props;
 
-    return (
-        <LINKBAR>
-            {
-                links && links.map((link, i) => (
-                    <LINKCARD key={i}>
-                        <ICON style={style && style.iconStyle} icon={
-                            link.name === 'facebook' ? faFacebook
-                                : link.name === 'github' ? faGithub
-                                    : link.name === 'twitter' ? faTwitter
-                                        : link.name === 'linkedin' ? faLinkedinIn
-                                            : link.name === 'instagram' ? faInstagram
-                                                : null
-                        } />
-                        <LINK>{link.name}</LINK>
-                    </LINKCARD>
-                ))
-            }
-        </LINKBAR>
-    )
+    if (links && links) {
+        return (
+            <LINKBAR>
+                {
+                    links && links.map((link, i) => (
+                        <LINKCARD key={i}>
+                            <ICON style={style && style.iconStyle} icon={
+                                link.name === 'facebook' ? faFacebook
+                                    : link.name === 'github' ? faGithub
+                                        : link.name === 'twitter' ? faTwitter
+                                            : link.name === 'linkedin' ? faLinkedinIn
+                                                : link.name === 'instagram' ? faInstagram
+                                                    : null
+                            } />
+                            <LINK>{link.name}</LINK>
+                        </LINKCARD>
+                    ))
+                }
+            </LINKBAR>
+        )
+    } else {
+        <PLACEHOLDER onClick={OnClickCallback} item_is_null={true} >
+            <PLUS_ICON style={style && style.plusIconSyle} icon={faPlus} />
+            <p style={{ width: '100%', textAlign: 'center' }}>Add an link</p>
+        </PLACEHOLDER>
+    }
 }
 
 export default LinkBar
