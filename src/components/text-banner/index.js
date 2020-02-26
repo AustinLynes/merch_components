6d18c419aabe946510b0f3902fa3253f164c4058
AustinLynes <austinlynes94@gmail.com>
2020-02-25 19:19:20 -0800

    tuesday -- adding default style footer links, edit to the link component --

--- src/components/text-banner/index.js
+++ src/components/text-banner/index.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import { BANNER, MESSAGE, INPUT } from './style';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'
 
 const TextBanner = (props) => {
     const { style, message, size, ...rest } = props
-
+    const [edit, setEdit] = useState(false)
     return (
 
         <BANNER style={style && style.wrapperStyle}>
             {!edit ?
                 <MESSAGE style={style && style.wrapperStyle} >{message}</MESSAGE>
                 : <INPUT style={style && style.inputStyle} onChange={OnChangeCallback} value={name} />}
+            <EDIT_ICON icon={faPencilAlt} onClick={OnClickCallback} />
+            
         </BANNER>
     )
 }
--- src/components/text-banner/style.js
+++ src/components/text-banner/style.js
@@ -16,7 +16,31 @@ ${props=>{
     `
 }}
 `
-
+export const EDIT_ICON = styled(FontAwesomeIcon)`
+width:5%;
+color:lightgrey;
+margin:0 10px;
+${props => {
+        if (props.style) return `
+             position:${props.style.position};
+            bottom:${props.style.bottomPos};
+            top:${props.style.topPos};
+            right:${props.style.rightPos};
+            left:${props.style.leftPos};
+            padding:${props.style.padding};
+            width:${props.style.width};
+            font-size:${props.style.fontSize};
+            color: black;  // this is black as a fallback... 
+            -webkit-text-fill-color: ${props.style.textFillColor}; 
+            -webkit-text-stroke-width: ${props.style.strokeWidth};
+            -webkit-text-stroke-color: ${props.style.strokeColor};
+            text-align:${props.style.textAlign};
+            border:${props.style.border};
+            background:${props.style.backgroundColor};
+            margin:${props.style.margin};
+    `
+    }}
+`
 export const MESSAGE = styled.h2`
 width:90%;
 margin:0 auto;
