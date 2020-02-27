84582e4626eaaee2b7dfcee1d473808b4bccb979
AustinLynes <austinlynes94@gmail.com>
2020-02-26 20:49:44 -0800

    guess im adding media querys

--- src/components/footer/style.js
+++ src/components/footer/style.js
@@ -41,6 +41,8 @@ export const LINK = styled.button`
     background:#000;
     color:white;
     @media (max-width:500px) {
+  
+
         padding-top:20px;
     }
     ${props => {
@@ -70,7 +72,9 @@ export const LINK_WRAPPER = styled.div`
     font-size:1.8rem;
     background:#000;
     color:white;
+    height:75px;
     @media (max-width:500px) {
+       
         width:100%;
     }
     /* any overriding styles will be displayed here  */
