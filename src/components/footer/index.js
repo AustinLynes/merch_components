7714bb3481556f366c6294a87a4c0b05ecbd4ceb
AustinLynes <austinlynes94@gmail.com>
2020-02-27 20:13:36 -0800

    weekeend updates -- adding default style a item --

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,30 +6,21 @@ import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'
 export const Footer = (props) => {
     const { style, links, onChangeCallback, ...rest } = props;
 
-    const [editables, setEditables] = useState([]);
+    const [edit, setEdit] = useState(false);
 
     const OnChangeCallback = (e) => {
         e.preventDefault();
         onChangeCallback && onChangeCallback(e);
     }
-    useEffect(() => {
-        setEditables(links && links.map((link, i) => {
-            return { id: i, edit: false }
-        }))
-        console.log(`*******  ${editables}`)
-    }, [])
     const OnClickCallback = (e) => {
-        console.log(`*******  ${JSON.stringify(editables)}  *******`)
-        const editables_flipped_val = editables.map((e, i) => {
-            e.edit = !e.edit;
-        })
-        setEditables(editables_flipped_val)
+        
+        setEdit(!edit)
     }
     return (
         <FOOTER>
             <EDIT_ICON icon={faPencilAlt} onClick={OnClickCallback} />
             {
-                links && links.map((link, i) => (<Link key={i + '_lf'} onChangeCallback={OnChangeCallback} style={link.style} name={link.name} edit={editables && editables[i - 1].edit} />))
+                links && links.map((link, i) => (<Link key={i + '_lf'} onChangeCallback={OnChangeCallback} style={link.style} name={link.name} edit={edit} />))
             }
         </FOOTER>
     )
