2ff7fcbbef5ed9261053a62488a0a52ddeb50926
AustinLynes <austinlynes94@gmail.com>
2020-02-27 20:29:13 -0800

    weekeend updates -- adding default style a item --

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,7 +5,7 @@ import { faSearch, faShoppingCart, faPencilAlt } from '@fortawesome/free-solid-s
 
 const Header = (props) => {
 
-    const { style, title,query, logoSrc, cartCount, handleChangeCallback, ...rest } = props;
+    const { style, title, query, logoSrc, cartCount, handleChangeCallback, ...rest } = props;
     const [edit, setEdit] = useState(false)
     const OnChangeCallback = (e) => {
         e.preventDefault();
@@ -15,15 +15,15 @@ const Header = (props) => {
         <HEADER style={style && style.wrapperStyle}>
             <LOGO style={style && style.logoStyle} src={logoSrc} />
             <TITLE_WRAPPER>
+                <EDIT_ICON icon={faPencilAlt} onClick={() => { setEdit(!edit) }} />
                 {!edit ?
                     <TITLE style={style && style.titleStyle} onChange={OnChangeCallback}>{title && title}</TITLE>
-                    : <INPUT value={title} onChange={OnChangeCallback}/>
+                    : <INPUT value={title} onChange={OnChangeCallback} />
                 }
-                <EDIT_ICON icon={faPencilAlt} onClick={() => { setEdit(!edit) }} />
             </TITLE_WRAPPER>
             <SEARCH_BAR style={style && style.searchBarStyle}>
-                <QUERY style={style && style.queryStyle} value={query}/>
-                <SEARCH_BUTTON style={style && style.searchIconStyle} icon={faSearch}  />
+                <QUERY style={style && style.queryStyle} value={query} />
+                <SEARCH_BUTTON style={style && style.searchIconStyle} icon={faSearch} />
             </SEARCH_BAR>
             <CART style={style && style.cartStyle}>
                 <ICON style={style && style.cartIconStyle} icon={faShoppingCart} />
--- src/components/text-banner/index.js
+++ src/components/text-banner/index.js
@@ -3,25 +3,25 @@ import { BANNER, MESSAGE, INPUT, EDIT_ICON } from './style';
 import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'
 
 const TextBanner = (props) => {
-    const { style, message, size,onChangeCallback, onClickCallback, ...rest } = props
+    const { style, message, size, onChangeCallback, onClickCallback, ...rest } = props
     const [edit, setEdit] = useState(false)
-    const OnClickCallback = (e)=>{
+    const OnClickCallback = (e) => {
         e.preventDefault();
         setEdit(!edit);
         onClickCallback && onClickCallback(e);
     }
-    const OnChangeCallback = (e)=>{
+    const OnChangeCallback = (e) => {
         e.preventDefault();
         onChangeCallback && onChangeCallback(e);
     }
     return (
 
         <BANNER style={style && style.wrapperStyle}>
+            <EDIT_ICON icon={faPencilAlt} onClick={OnClickCallback} />
             {!edit ?
                 <MESSAGE style={style && style.wrapperStyle} >{message}</MESSAGE>
                 : <INPUT style={style && style.inputStyle} onChange={OnChangeCallback} value={message} />}
-            <EDIT_ICON icon={faPencilAlt} onClick={OnClickCallback} />
-            
+
         </BANNER>
     )
 }
