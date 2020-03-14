2a2dfd06c9d1e3d7c468a13d8686bd24f309dd14
AustinLynes <austinlynes94@gmail.com>
2020-03-13 17:19:49 -0700

    revert back to terinarys

--- src/components/link-bar/index.js
+++ src/components/link-bar/index.js
@@ -5,27 +5,7 @@ import { Placeholder } from '../placeholder/index.js';
 
 const LinkBar = (props) => {
     const { links, style, ...rest } = props;
-    var _n = null;
-
-    const findName = (link) => {
-        // alert(`THIS IS A DEBUG MESSAGE... FINDING NAME ${link.name}`)
-        let _link;
-        switch (link.name) {
-            case 'facebook':
-                return _link = faFacebook;
-            case 'github':
-                return _link = faGithub;
-            case 'twitter':
-                return _link = faTwitter;
-            case 'linkedin':
-                return _link = faLinkedinIn;
-            case 'instagram':
-                return _link = faInstagram;
-            default:
-                break;
-        }
-        return _link;
-    }
+   
     if (links && links) {
         return (
             <LINKBAR data-testid='wrapper'>
@@ -34,7 +14,13 @@ const LinkBar = (props) => {
                         <LINKCARD data-testid='link-cards' key={i}>
                             {_n = findName(link)}
                             {console.log(_n)}
-                            <ICON data-testid='link-card-icon' style={style && style.iconStyle} icon={_n.iconName} />
+                            <ICON data-testid='link-card-icon' style={style && style.iconStyle} icon={
+                                link.name === 'facebook' ? faFacebook :
+                                    link.name === 'github' ? faGitHub :
+                                        link.name === 'twitter' ? faTwitter :
+                                            link.name === 'instagram' ? faInstagram :
+                                                link.name === 'linkedin' ? faLinkedinIn : null
+                            } />
                             <LINK >{link.name}</LINK>
                         </LINKCARD>
                     ))
