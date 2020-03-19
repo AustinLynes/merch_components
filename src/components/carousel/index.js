cb5cfa4b9ec758c11e285b4136c70b21c3c05715
AustinLynes <austinlynes94@gmail.com>
2020-03-18 21:20:39 -0700

    more

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import { CAROSEL as CAROUSEL, IMAGE, PLACEHOLDER_CAROSEL, PLACEHOLDER, ICON } from './Style';
 import { faAngleLeft, faAngleRight, faImages } from '@fortawesome/free-solid-svg-icons';
-
+                        //    3         3
 export const increment = (_count, _repeatVal) => {
+                                          
     _count >= _repeatVal - 1 ? _count = 0 : _count += 1
-    return _count += 1
+    return _count 
 }
 export const decrement = (_count, _repeatVal) => {
 
     _count <= 0 ? _count = _repeatVal : _count -= 1
-    return _count -= 1
+    return _count 
 }
 const Carousel = (props) => {
     const { images, style, ...rest } = props;
@@ -18,7 +19,7 @@ const Carousel = (props) => {
     if (images && images.length > 0) {
         return (
             <CAROUSEL data-testid='wrapper' style={style && style.wrapperStyle}>
-                <ICON data-testid={'icon decrement'} style={style && style.leftIconStyle} icon={faAngleLeft} onClick={() => setCount(increment(count, images.length))} />
+                <ICON data-testid={'icon decrement'} style={style && style.leftIconStyle} icon={faAngleLeft} onClick={() => setCount(increment(count, images.length - 1))} />
                 {
                     images.map((image, i) => (
                         i === count && <IMAGE draggable={false} id={`${i}_im`} data-testid='image' key={`${i}_im`} style={style && style.imageStyle} src={image} />)
