d06994fd648d9b7c8606d24d14c0d220625fa61f
AustinLynes <austinlynes94@gmail.com>
2020-03-19 13:01:03 -0700

    fixing the carousel

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -11,6 +11,7 @@ export const increment = (_count, _repeatVal) => {
 export const decrement = (_count, _repeatVal) => {
 
     _count < 0 ? _count = _repeatVal : _count -= 1
+    console.log(_count);
     return _count 
 }
 
@@ -21,13 +22,13 @@ const Carousel = (props) => {
     if (images && images.length > 0) {
         return (
             <CAROUSEL data-testid='wrapper' style={style && style.wrapperStyle}>
-                <ICON data-testid={'icon decrement'} style={style && style.leftIconStyle} icon={faAngleLeft} onClick={() => setCount(increment(count, images.length - 1))} />
+                <ICON data-testid={'icon decrement'} style={style && style.leftIconStyle} icon={faAngleLeft} onClick={() => setCount(decrement(count, images.length - 1))} />
                 {
                     images.map((image, i) => (
                         i === count && <IMAGE draggable={false} id={`${i}_im`} data-testid='image' key={`${i}_im`} style={style && style.imageStyle} src={image} />)
                     )
                 }
-                <ICON data-testid={'icon increment'} style={style && style.rightIconStyle} icon={faAngleRight} onClick={() => setCount(decrement(count, 0))} />
+                <ICON data-testid={'icon increment'} style={style && style.rightIconStyle} icon={faAngleRight} onClick={() => setCount(increment(count, images.length - 1))} />
             </CAROUSEL>
         )
     } else {
