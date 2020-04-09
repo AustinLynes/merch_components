5b25c7b4615860bf86f9eb433315fc57462c1123
AustinLynes <austinlynes94@gmail.com>
2020-04-08 20:20:53 -0700

    more

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     const [logo, setLogo] = useState(null);
     const setLogoSrc = (e) => {
         console.log(e.target.files[0])
-        setLogo(e.target.files[0])
+        setLogo(e.target.files[0].name)
         console.log(logo)
     }
     return (
